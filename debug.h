58d185e88e01d9223d6e3a73aac067cfda721ea7
Chuanheng Feng <fengchuanheng@sjtu.edu.cn>
2020-10-25 15:12:21 +0800

    Improve degub.h

--- debug.h
+++ debug.h
@@ -2,14 +2,14 @@
 #define PFX "example_mod: "
 
 #ifdef DEBUG
-    #define DEBUG_LOG(...)   printk(PFX __VA_ARGS__)
+    #define DEBUG_LOG(format, ...)   printk(PFX "[%s:%d]: " format, __func__, __LINE__, ##__VA_ARGS__)
 #else
     #define DEBUG_LOG(...) 
 #endif
 
 #define CHUANHENG
 #if defined(DEBUG) && defined(CHUANHENG)
-    #define CH_DEBUG(...)   DEBUG_LOG(PFX __VA_ARGS__)
+    #define CH_DEBUG(format, ...)   DEBUG_LOG(PFX format, ##__VA_ARGS__)
 #else
     #define CH_DEBUG(...) 
 #endif
