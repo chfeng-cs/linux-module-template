4ae38176ca6ed1d13aa1a43838f1e5a451189502
Chuanheng Feng <fengchuanheng@sjtu.edu.cn>
2020-10-26 13:20:14 +0800

    example.c

--- example.c
+++ example.c
@@ -25,31 +25,35 @@ MODULE_DESCRIPTION("Example Module");
 MODULE_LICENSE("Dual BSD/GPL");
 
 
-static void mtest_list_vma(void)
+static void mtest_list_vma(struct task_struct *task)
 {
-	struct mm_struct *mm = current->mm;
+	struct mm_struct *mm;
 	struct vm_area_struct *vma;
+	int i = 0;
+	char property[4] = "---";
+	if (task == NULL) {
+		task = current;
+	}
+	mm = task->mm;
 	down_read(&mm->mmap_sem);
 	DEBUG_LOG("list_vma");
-	for (vma=mm->mmap; vma; vma=vma->vm_next) {
-		char property[4] = {0};
+	for (vma=mm->mmap; vma; vma=vma->vm_next, i++) {
 		if (vma->vm_flags & VM_READ) {
 			property[0] = 'r';
-		} else {
-			property[0] = '-';
 		}
 		if (vma->vm_flags & VM_WRITE) {
 			property[1] = 'w';
-		} else {
-			property[1] = '-';
 		}
 		if (vma->vm_flags & VM_EXEC) {
 			property[2] = 'x';
-		} else {
-			property[2] = '-';
 		}
-		// print_value(vma, 0x%p);
-		printk("VMA 0x%lx-0x%lx %s", vma->vm_start, vma->vm_end, property);
+		// printk("VMA 0x%lx-0x%lx %s", vma->vm_start, vma->vm_end, property);
+		if ( i == 10) break;
+	}
+	if (vma != NULL) {
+		DEBUG_LOG("VMA 0x%lx-0x%lx %s", vma->vm_start, vma->vm_end, property);
+	} else {
+		DEBUG_LOG("end");
 	}
 	up_read(&mm->mmap_sem);
 }
@@ -76,6 +80,7 @@ static void mtest_list_vma111(void)
 	DEBUG_LOG("pid: %d", mc->pid);
 	// DEBUG_LOG("files: %p", mc->files);
 } 
+
 ssize_t proc_write (struct file *f, const char __user *user_buff, size_t len, loff_t * offset) {
 	char buf[100];
 	copy_from_user(buf, user_buff, len);
@@ -83,7 +88,6 @@ ssize_t proc_write (struct file *f, const char __user *user_buff, size_t len, lo
 	print_value(buf, %s);
 	print_value(len, %u);
 	print_value(*offset, %u);
-	mtest_list_vma();
 	return len;
 }
 struct file_operations proc_fp = {
@@ -92,13 +96,18 @@ struct file_operations proc_fp = {
 struct proc_dir_entry *entry;
 static int __init krping_init(void)
 {
-	// add_wait_queue();
-	// printk(KERN_ERR PFX "Invalid Parameter");
+	struct task_struct *p;
+	for_each_process(p) {
+		if (p->pid == 10986) {
+			DEBUG_LOG("addr: 0x%p\tpid: %-5d\tname: %s", p, p->pid, p->comm);
+			break;
+		}
+	}
 	entry = proc_create("chfeng", 0666, NULL, &proc_fp);
 	if (entry == NULL) {
 		DEBUG_LOG("Create failed.");
 	}
-	mtest_list_vma();
+	mtest_list_vma(p);
 	DEBUG_LOG("example_mod_start\n");
 	return 0;
 }
